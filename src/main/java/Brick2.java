5326df3636ef047efff9ceb1b7f1cf4c70c35127
April Yao <scysy2@nottingham.ac.uk>
2021-12-27 20:50:49 +0000

    renamed the class to match "clay=1, steel=2, cement=3", add cracks to Brick2.java to make the impact more visible to the user

--- src/main/java/Brick2.java
+++ src/main/java/Brick2.java
@@ -1,25 +1,31 @@
 //package code;
 
 import java.awt.*;
-import java.awt.Point;
 import java.awt.geom.Point2D;
+import java.awt.geom.GeneralPath;
+import java.util.Random;
 
 
+// renamed the class to match "clay=1, steel=2, cement=3"
 public class Brick2 extends Brick {
 
-	private static final String NAME = "Clay Brick";
-	private static final Color DEF_INNER = new Color(178, 34, 34).darker();
-	private static final Color DEF_BORDER = Color.GRAY;
-	private static final int CLAY_STRENGTH = 1;
+	private static final String NAME = "Steel Brick";
+	private static final Color DEF_INNER = new Color(203, 203, 201);
+	private static final Color DEF_BORDER = DEF_INNER.darker().darker().darker();		// original black doesn't look so harmonious
+	private static final int STEEL_STRENGTH = 2;	// as steel, it should have middle strength
+	private static final double STEEL_PROBABILITY = 0.4;
 
-
-
-
-
+	private Random rnd;
+	private Shape brickFace;
+	private Crack crack;		// set crack, more visible for user
 
 	public Brick2(Point point, Dimension size){
-		super(NAME,point,size,DEF_BORDER,DEF_INNER,CLAY_STRENGTH);
+		super(NAME,point,size,DEF_BORDER,DEF_INNER,STEEL_STRENGTH);
+		rnd = new Random();
+		crack = new Crack(DEF_CRACK_DEPTH,DEF_STEPS);
+		brickFace = super.brickFace;
 	}
+
 
 	@Override
 	protected Shape makeBrickFace(Point pos, Dimension size) {
@@ -28,8 +34,40 @@ public class Brick2 extends Brick {
 
 	@Override
 	public Shape getBrick() {
-		return super.brickFace;
+		return brickFace;
 	}
 
+	private void updateBrick(){
+		if(!super.isBroken()){
+			GeneralPath gp = crack.draw();
+			gp.append(super.brickFace,false);
+			brickFace = gp;
+		}
+	}
+
+	@Override
+	public  boolean setImpact(Point2D point , int dir){
+		if(super.isBroken())
+			return false;
+		super.impact();
+		if(!super.isBroken()){
+			crack.makeCrack(point,dir);
+			updateBrick();
+			return false;
+		}
+		return true;
+	}
+
+	public void impact(){
+		if(rnd.nextDouble() < STEEL_PROBABILITY){
+			super.impact();
+		}
+	}
+
+	public void repair(){
+		super.repair();
+		crack.reset();
+		brickFace = super.brickFace;
+	}
 
 }
--- src/main/java/Brick3.java
+++ src/main/java/Brick3.java
@@ -1,27 +1,27 @@
 //package code;
 
 import java.awt.*;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
-import java.util.Random;
 
-
+// renamed the class to match "clay=1, steel=2, cement=3"
 public class Brick3 extends Brick {
 
-	private static final String NAME = "Steel Brick";
-	private static final Color DEF_INNER = new Color(203, 203, 201);
-	private static final Color DEF_BORDER = Color.BLACK;
-	private static final int STEEL_STRENGTH = 1;
-	private static final double STEEL_PROBABILITY = 0.4;
 
-	private Random rnd;
+	private static final String NAME = "Cement Brick";
+	private static final Color DEF_INNER = new Color(147, 147, 147);
+	private static final Color DEF_BORDER = new Color(217, 199, 175);
+	private static final int CEMENT_STRENGTH = 3;
+
+	private Crack crack;
 	private Shape brickFace;
 
+
 	public Brick3(Point point, Dimension size){
-		super(NAME,point,size,DEF_BORDER,DEF_INNER,STEEL_STRENGTH);
-		rnd = new Random();
+		super(NAME,point,size,DEF_BORDER,DEF_INNER,CEMENT_STRENGTH);
+		crack = new Crack(DEF_CRACK_DEPTH,DEF_STEPS);
 		brickFace = super.brickFace;
 	}
-
 
 	@Override
 	protected Shape makeBrickFace(Point pos, Dimension size) {
@@ -29,21 +29,35 @@ public class Brick3 extends Brick {
 	}
 
 	@Override
+	public boolean setImpact(Point2D point, int dir) {
+		if(super.isBroken())
+			return false;
+		super.impact();
+		if(!super.isBroken()){
+			crack.makeCrack(point,dir);
+			updateBrick();
+			return false;
+		}
+		return true;
+	}
+
+
+	@Override
 	public Shape getBrick() {
 		return brickFace;
 	}
 
-	public  boolean setImpact(Point2D point , int dir){
-		if(super.isBroken())
-			return false;
-		impact();
-		return  super.isBroken();
-	}
-
-	public void impact(){
-		if(rnd.nextDouble() < STEEL_PROBABILITY){
-			super.impact();
+	private void updateBrick(){
+		if(!super.isBroken()){
+			GeneralPath gp = crack.draw();
+			gp.append(super.brickFace,false);
+			brickFace = gp;
 		}
 	}
 
+	public void repair(){
+		super.repair();
+		crack.reset();
+		brickFace = super.brickFace;
+	}
 }
